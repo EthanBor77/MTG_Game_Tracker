30cc3a7b12b78a61de3824b8a36ee13c7ae023f6
Ethan <eborror@oakland.edu>
2026-05-07 19:46:48 -0400

    modified:   pages/fun_stats.py

--- pages/fun_stats.py
+++ pages/fun_stats.py
@@ -3,7 +3,14 @@ import streamlit as st
 # Longest game
 # Shortest game
 # Win con distribution
-# New page to view all the logged games in a more readable format (with the game details and player/deck info)
+
 
 st.set_page_config(page_title="Fun Stats", layout="wide")
 st.title("🎉 There's Nothing Here Yet! 🎉")
+
+st.title("Things I'm Excited to Add Soon:")
+st.markdown("""
+- **Longest & Shortest Games**: See which games went the distance and which ones were lightning fast.
+- **Win Condition Breakdown**: A visual breakdown of how games are being won in our playgroup.
+- **Largest First Blood to game end difference**: Which games had the biggest survival gap?
+""")
