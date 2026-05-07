42713e60b29dea3589e13c281d3091ad2150453f
Ethan <eborror@oakland.edu>
2026-05-07 19:35:14 -0400

    modified:   pages/fun_stats.py

--- pages/fun_stats.py
+++ pages/fun_stats.py
@@ -1,4 +1,9 @@
+import streamlit as st
+
 # Longest game
 # Shortest game
 # Win con distribution
 # New page to view all the logged games in a more readable format (with the game details and player/deck info)
+
+st.set_page_config(page_title="Fun Stats", layout="wide")
+st.title("🎉 There's Nothing Here Yet! 🎉")
